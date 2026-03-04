70f74d6bc992b6f30cc1f76e5ef3c372152e73b1
Saketh Macha <saketh.macha@cognine.com>
2026-03-04 12:53:53 +0530

    save

--- MovieBooking.Web/Program.cs
+++ MovieBooking.Web/Program.cs
@@ -17,11 +17,6 @@ namespace MovieBooking.Web
                 var Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
                 Client.BaseAddress = new Uri(Configuration["ApiSettings:BaseUrl"]!);
             });
-            //builder.Services.AddHttpClient<ISuperAdminMvcService,SuperAdminMvcService>((ServiceProvider, Client) =>
-            //{
-            //    var Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
-            //    Client.BaseAddress = new Uri(Configuration["ApiSettings:BaseUrl"]!);
-            //});
             builder.Services.AddHttpClient<IBookingMvcService, BookingMvcService>((ServiceProvider, Client) =>
             {
                 var Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
